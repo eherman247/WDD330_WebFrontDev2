c1282f9fcbaba218389bfcd75797d232921feed4
eherman247 <59893598+eherman247@users.noreply.github.com>
2021-05-01 16:55:37 -0600

    Modified week 2 notes to include the nav

--- week2/scripts/week2.js
+++ week2/scripts/week2.js
@@ -1,3 +1,38 @@
+const links = [{
+    label: "Week 1 notes",
+    url: "../week1/index.html"
+},
+{
+    label: "Week 2 notes",
+    url: "../week2/index.html"
+},
+{
+    label: "Week 3 notes",
+    url: "../week3/index.html"
+}
+];
+
+// get pointer to <ul> element
+let ul = document.querySelector("ul");
+
+
+
+
+
+links.forEach(
+link => {
+    ul.innerHTML += 
+    `
+        <li>
+            <a href="${link.url}">${link.label}</a>
+        </li>
+    `;
+}
+);
+
+
+
+
 // //A bit of test code from class
 // let fName = "Ethan";
 // let lName = "Herman";
